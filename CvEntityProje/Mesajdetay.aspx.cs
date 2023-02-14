0986700a6049ac7ce4a0a194fffd070198c2bcea
aozturk <aozturk@AHMETOZTURK>
2023-02-14 15:58:34 +0300

    Add project files.

---
+++ CvEntityProje/Mesajdetay.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CvEntityProje
+{
+    public partial class Mesajdetay : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DBCVENTİTYEntities1 db = new DBCVENTİTYEntities1();
+            int x= int.Parse(Request.QueryString["ID"]);
+            var mesaj = db.TBLILETISIM.Find(x);
+            TxtAdSoyad.Text = mesaj.ADSOYAD;
+            TxtMail.Text = mesaj.MAIL;
+            TxtKonu.Text = mesaj.KONU;
+            TxtMesaj.Text = mesaj.MESAJ;
+        }
+    }
+}
